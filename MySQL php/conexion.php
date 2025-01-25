c3773f6ab8dbc0038f7a8659ac2194051f58655a
SamuelPadial <114154158+SamuelPadial@users.noreply.github.com>
2025-01-25 17:09:00 +0100

    Update conexion.php

--- MySQL php/conexion.php
+++ MySQL php/conexion.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "sql308.thsite.top";
-$username = "thsi_38097495";
-$password = "?X1!KS8e";
+$servername =  "***";
+$username = "***";
+$password = "***";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=thsi_38097495_bdejemplo", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=***", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
